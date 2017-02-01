55123b6272bd112c83e8e8ecf9683d52975fe943
trebuhd <hubert.dworczynski@gmail.com>
2017-02-01 21:41:26 +0100

    bask to Polish

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -2,6 +2,6 @@ package util;
 
 public class Constants {
     public static final String BOT_USERNAME = "say_it_bot";
-    public static final String SPEECH_API_URL_PL = "https://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&tl=pl&q=<MESSAGE>&tl=ja";
+    public static final String SPEECH_API_URL_PL = "https://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&tl=pl&q=<MESSAGE>&tl=pl";
     public static final String MOZILLA_UA = "Mozilla/5.0 (Windows NT 5.1; rv:19.0) Gecko/20100101 Firefox/19.0";
 }
